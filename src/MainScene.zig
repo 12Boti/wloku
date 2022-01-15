d8435e5289657d0731dfd9c769c59f4fe323df01
Olle Boti <olle12boti+git@gmail.com>
2022-01-15 15:20:15 +0100

    bullet shooting

--- src/MainScene.zig
+++ src/MainScene.zig
@@ -2,10 +2,14 @@ const Self = @This();
 const std = @import("std");
 const w4 = @import("wasm4.zig");
 const Sprite = @import("Sprite.zig");
+const ArrayVec = @import("ArrayVec.zig").ArrayVec;
 
-const walls_size = 100;
-const walls_color = 3;
-const Walls = std.PackedIntArray(u1, walls_size * walls_size);
+const wallsSize = 100;
+const wallColor = 3;
+const Walls = std.PackedIntArray(u1, wallsSize * wallsSize);
+const bulletSpeed = 0.8;
+const bulletColor = 1;
+const shootDelay = 60;
 
 player1: Tank = Tank{
     .x = 10,
@@ -14,11 +18,13 @@ player1: Tank = Tank{
     .vy = 0,
     .speed = 0.5,
     .rot = 0,
+    .shootTimer = 0,
     .sprite = &Sprite.load("tank"),
 },
 // use `std.mem.zeroes` here because it makes compilation much faster than
 // looping and setting all elements to 0
 walls: Walls = std.mem.zeroes(Walls),
+bullets: ArrayVec(Bullet, 128) = .{},
 // TODO: gather entropy from player input
 rng: std.rand.DefaultPrng = std.rand.DefaultPrng.init(0xdeadbeef),
 
@@ -29,23 +35,25 @@ pub fn init() Self {
 }
 
 pub fn update(s: *Self) void {
-    updatePlayer(&s.player1, w4.GAMEPAD1.*);
+    s.updateBullets();
+    s.updatePlayer(&s.player1, w4.GAMEPAD1.*);
 }
 
 pub fn draw(s: Self) void {
     w4.DRAW_COLORS.* = 2;
     w4.text("Hello from Zig!", 10, 10);
     s.drawWalls();
+    s.drawBullets();
     s.player1.draw();
 }
 
 fn generateWalls(s: *Self) void {
     var x: usize = 0;
-    while (x < walls_size) : (x += 1) {
+    while (x < wallsSize) : (x += 1) {
         var y: usize = 0;
-        while (y < walls_size) : (y += 1) {
+        while (y < wallsSize) : (y += 1) {
             if (s.rng.random().float(f32) < 0.5) {
-                s.walls.set(x + y * walls_size, 1);
+                s.walls.set(x + y * wallsSize, 1);
             }
         }
     }
@@ -53,17 +61,42 @@ fn generateWalls(s: *Self) void {
 
 fn drawWalls(s: Self) void {
     var x: usize = 0;
-    while (x < walls_size) : (x += 1) {
+    while (x < wallsSize) : (x += 1) {
         var y: usize = 0;
-        while (y < walls_size) : (y += 1) {
-            if (s.walls.get(x + y * walls_size) == 1) {
-                w4.setPixel(x + 30, y + 30, walls_color);
+        while (y < wallsSize) : (y += 1) {
+            if (s.walls.get(x + y * wallsSize) == 1) {
+                w4.setPixel(x + 30, y + 30, wallColor);
             }
         }
     }
 }
 
-fn updatePlayer(tank: *Tank, pad: w4.Gamepad) void {
+fn updateBullets(s: *Self) void {
+    var i: usize = 0;
+    while (i < s.bullets.size) {
+        const b = &s.bullets.buf[i];
+        if (b.x < 0 or b.x >= w4.CANVAS_SIZE or b.y < 0 or b.y >= w4.CANVAS_SIZE) {
+            _ = s.bullets.swapRemove(i);
+            continue;
+        }
+        b.x += b.vx;
+        b.y += b.vy;
+        i += 1;
+    }
+}
+
+fn drawBullets(s: Self) void {
+    var i: usize = 0;
+    while (i < s.bullets.size) : (i += 1) {
+        const b = s.bullets.buf[i];
+        if (b.x < 0 or b.x >= w4.CANVAS_SIZE or b.y < 0 or b.y >= w4.CANVAS_SIZE)
+            continue;
+        w4.setPixel(@floatToInt(u32, b.x), @floatToInt(u32, b.y), bulletColor);
+    }
+}
+
+fn updatePlayer(s: *Self, tank: *Tank, pad: w4.Gamepad) void {
+    // movement
     if (tank.vy == 0) {
         tank.vx = 0;
         if (pad.left()) {
@@ -82,6 +115,13 @@ fn updatePlayer(tank: *Tank, pad: w4.Gamepad) void {
             tank.vy += tank.speed;
         }
     }
+    var newx = tank.x + tank.vx;
+    var newy = tank.y + tank.vy;
+    if (!isColliding(newx + 1, newy + 1, 5, 5)) {
+        tank.x = newx;
+        tank.y = newy;
+    }
+    // rotation
     if (tank.vy < 0) {
         tank.rot = 0;
     } else if (tank.vx < 0) {
@@ -91,11 +131,37 @@ fn updatePlayer(tank: *Tank, pad: w4.Gamepad) void {
     } else if (tank.vx > 0) {
         tank.rot = 3;
     }
-    var newx = tank.x + tank.vx;
-    var newy = tank.y + tank.vy;
-    if (!isColliding(newx + 1, newy + 1, 5, 5)) {
-        tank.x = newx;
-        tank.y = newy;
+    // shooting
+    tank.shootTimer -|= 1;
+    if (tank.shootTimer == 0 and pad.b1()) {
+        tank.shootTimer = shootDelay;
+        var bullet = Bullet{
+            .x = tank.x + 3,
+            .y = tank.y + 3,
+            .vx = 0,
+            .vy = 0,
+        };
+        switch (tank.rot) {
+            0 => {
+                bullet.y -= 3;
+                bullet.vy = -bulletSpeed;
+            },
+            1 => {
+                bullet.x -= 3;
+                bullet.vx = -bulletSpeed;
+            },
+            2 => {
+                bullet.y += 3;
+                bullet.vy = bulletSpeed;
+            },
+            3 => {
+                bullet.x += 3;
+                bullet.vx = bulletSpeed;
+            },
+        }
+        s.bullets.append(bullet) catch {
+            w4.trace("warning: too many bullets");
+        };
     }
 }
 
@@ -106,6 +172,13 @@ fn isColliding(x: f32, y: f32, w: f32, h: f32) bool {
     return false;
 }
 
+const Bullet = struct {
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
+};
+
 const Tank = struct {
     x: f32,
     y: f32,
@@ -113,6 +186,7 @@ const Tank = struct {
     vy: f32,
     speed: f32,
     rot: u2,
+    shootTimer: u32,
     sprite: *const Sprite,
 
     fn draw(self: Tank) void {
