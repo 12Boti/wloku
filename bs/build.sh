6b160f1bfd8344dc49c62fc545df601b4d45f6b8
Olle Boti <olle12boti+git@gmail.com>
2022-01-21 18:48:57 +0100

    tweak build commands

--- bs/build.sh
+++ bs/build.sh
@@ -1,6 +1,7 @@
 set -eu
 bash bs/assets.sh
 mkdir -p build
-zig build --cache-dir build/zig-cache -p build
-wasm-opt -Oz -o build/lib/cart.wasm build/lib/cart.wasm
+zig build --cache-dir build/zig-cache -p build --prominent-compile-errors
+wasm-opt -Oz -o build/lib/cart.wasm build/lib/cart.wasm \
+    --zero-filled-memory --strip-producers --converge
 echo "cart is $(stat -c '%s' build/lib/cart.wasm) bytes"
